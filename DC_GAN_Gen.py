4cc36dd9b61d255cdf3067f4dd5ebad19e0f247c
unknown <mastergolizadeh@gmail.com>
2020-11-16 19:49:58 +0330

    added readme

---
+++ DC_GAN_Gen.py
@@ -0,0 +1,67 @@
+import torch
+
+class Generator(torch.nn.Module):
+    '''
+    Generator Class
+    Values:
+        z_dim: the dimension of the noise vector, a scalar
+        im_chan: the number of channels of the output image, a scalar
+              (CelebA is rgb, so 3 is your default)
+        hidden_dim: the inner dimension, a scalar
+    '''
+    def __init__(self, z_dim=10, im_chan=3, hidden_dim=64):
+        super(Generator, self).__init__()
+        self.z_dim = z_dim
+        # Build the neural network
+        self.gen = torch.nn.Sequential(
+            self.make_gen_block(z_dim, hidden_dim * 8),
+            self.make_gen_block(hidden_dim * 8, hidden_dim * 4),
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
+        )
+
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a generator block of DCGAN;
+        a transposed convolution, a batchnorm (except in the final layer), and an activation.
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+        if not final_layer:
+            return torch.nn.Sequential(
+                torch.nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                torch.nn.BatchNorm2d(output_channels),
+                torch.nn.ReLU(inplace=True),
+            )
+        else:
+            return torch.nn.Sequential(
+                torch.nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                torch.nn.Tanh(),
+            )
+
+    def forward(self, noise):
+        '''
+        Function for completing a forward pass of the generator: Given a noise tensor, 
+        returns generated images.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, z_dim)
+        '''
+        x = noise.view(len(noise), self.z_dim, 1, 1)
+        return self.gen(x)
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    '''
+    return torch.randn(n_samples, z_dim, device=device)
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 this file contains the information of neural networks I have implemented
 
 tensor_data_visualization -->this notebook contains code which helps drawing tensor images in pytorch
+DC_GAN_Gen.py --> this file contain generator code for dc-gan
+GANS_Bias --> investigates the bias of datasets in gan training and the generated ones
